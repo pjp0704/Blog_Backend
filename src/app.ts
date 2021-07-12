7078b7a92cddeacdb5d3ceafc5ae6a8508f25ddd
pjp0704 <junhyeong25@gmail.com>
2021-07-12 19:50:37 +0900

    <Add> Middlewares for security/cors/log

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import config from './config';
+import hpp from 'hpp';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 const { MONGO_URL } = config;
+
+app.use(hpp());
+app.use(helmet());
+app.use(cors({ credentials: true }));
+app.use(morgan('dev'));
 
 mongoose
   .connect(MONGO_URL as string, {
