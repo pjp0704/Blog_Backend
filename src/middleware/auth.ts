42e9cba6863b6267d1bb7ad11758ee696226ee0b
pjp0704 <junhyeong25@gmail.com>
2021-07-14 14:13:18 +0900

    <Add> auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import config from '../config';
+
+const { JWT_SECRET } = config;
+
+const auth = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const token = req.header('Authentication');
+
+  if (!token) {
+    return res.status(401).json({ msg: 'No token provided' });
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET as string);
+    req.user = decoded;
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(401).json({ msg: 'Invalid token' });
+  }
+};
+
+export default auth;
